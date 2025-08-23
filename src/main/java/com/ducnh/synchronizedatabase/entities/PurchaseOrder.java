8dfd65fa5a3f7c69e1145d1f8a2a676fe3bb6cc6
Duc111095 <huynhduc111095@gmail.com>
2025-08-23 15:23:30 +0700

    finish task

--- src/main/java/com/ducnh/synchronizedatabase/entities/PurchaseOrder.java
+++ src/main/java/com/ducnh/synchronizedatabase/entities/PurchaseOrder.java
@@ -1,6 +1,5 @@
 package com.ducnh.synchronizedatabase.entities;
 
-import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,115 +8,131 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import java.util.Date;
+import org.hibernate.annotations.Nationalized;
+import java.math.BigDecimal;
+
+
 import lombok.Data;
 
 @Entity
-@Table
+@Table(name = "total_purchase_order")
 @Data
 public class PurchaseOrder {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 	
-	@Column
+	@Column(columnDefinition = "char(13)")
 	public String stt_rec;
 	
-	@Column
+	@Column(columnDefinition = "char(3)")
 	public String stt_rec0;
 	
-	@Column
+	@Column(columnDefinition = "char(24)")
 	public String so_ct_dt;
 	
-	@Column
+	@Column(columnDefinition = "char(8)")
 	public String ma_dvcs_dt;
 	
-	@Column
+	@Column(columnDefinition = "char(16)")
 	public String ma_nvbh;
 	
-	@Column
+	@Column(length = 256)
 	public String ten_nvbh;
 	
-	@Column
+	@Column(columnDefinition = "char(24)")
 	public String ma_vt;
 	
-	@Column
+	@Column(length = 256)
 	public String ref;
 	
-	@Column
+	@Column(length = 2048)
+	@Nationalized
 	public String ten_vt;
 	
-	@Column
+	@Column(columnDefinition = "char(24)")
 	public String nh_vt1;
 	
-	@Column
+	@Column(columnDefinition = "char(24)")
 	public String nh_vt2;
 	
-	@Column
+	@Column(columnDefinition = "char(24)")
 	public String nh_vt3;
 	
-	@Column
+	@Column(columnDefinition = "nchar(20)")
+	@Nationalized
 	public String dvt;
 	
 	@Column
-	public LocalDateTime ngay_dt;
+	public Date ngay_dt;
 	
-	@Column
+	@Column(columnDefinition = "varchar(16)")
 	public String ma_kh;
 	
-	@Column
+	@Column(columnDefinition = "nvarchar(1024)")
+	@Nationalized
 	public String ten_kh;
 	
 	@Column
-	public Double sl_dt;
+	public BigDecimal sl_dt;
 	
 	@Column
-	public Double sl_duyet_po;
+	public BigDecimal sl_duyet_po;
 	
 	@Column
-	public Double sl_nhap_tm;
+	public BigDecimal sl_nhap_tm;
 	
 	@Column
-	public Double sl_xuat_tm;
+	public BigDecimal sl_xuat_tm;
 	
 	@Column
-	public Double sl_cl_tm;
+	public BigDecimal sl_cl_tm;
 	
 	@Column
-	public LocalDateTime ngay_nhap_tm;
+	public Date ngay_nhap_tm;
 	
-	@Column
+	@Column(columnDefinition = "char(48)")
 	public String ma_lo_nhap_tm;
 	
 	@Column
-	public LocalDateTime han_lo_nhap_tm;
+	public Date han_lo_nhap_tm;
 	
-	@Column
+	@Column(columnDefinition = "char(8)")
 	public String ma_dvcs_nk;
 	
 	@Column
-	public Double sl_duyet_po_nk;
+	public BigDecimal sl_duyet_po_nk;
 	
 	@Column
-	public Double sl_tk_nk;
+	public BigDecimal sl_tk_nk;
 	
 	@Column
-	public Double sl_nhap_nk;
+	public BigDecimal sl_nhap_nk;
 	
 	@Column
-	public Double sl_xuat_nk;
+	public BigDecimal sl_xuat_nk;
 	
 	@Column
-	public Double sl_cl_nk;
+	public BigDecimal sl_cl_nk;
 	
 	@Column
-	public Double t_sl_cl;
+	public BigDecimal t_sl_cl;
 	
 	@Column
-	public LocalDateTime ngay_nhap_nk;
+	public Date ngay_nhap_nk;
 	
-	@Column
+	@Column(columnDefinition = "char(48)")
 	public String ma_lo_nhap_nk;
 	
 	@Column
-	public LocalDateTime han_lo_nhap_nk;
+	public Date han_lo_nhap_nk;
+	
+	@Override
+	public String toString() {
+		return "PurchaseOrder{" +
+				"id=" + id +
+				", ma_vt=" + ma_vt + "}";	
+	}
 }
--- src/main/java/com/ducnh/synchronizedatabase/service/PurchaseOrderService.java
+++ src/main/java/com/ducnh/synchronizedatabase/service/PurchaseOrderService.java
@@ -1,54 +1,69 @@
 package com.ducnh.synchronizedatabase.service;
 
-import java.util.List;
-
 import javax.persistence.Query;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import com.ducnh.synchronizedatabase.entities.PurchaseOrder;
 import com.ducnh.synchronizedatabase.utils.HibernateUtils;
+import java.util.List;
+import com.ducnh.synchronizedatabase.entities.*;
+import java.io.*;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class PurchaseOrderService {
-	public static void synchronizeData(final String dateFrom, final String dateTo, final String tmList, 
-			final String nkList, final int userId, final String unit, final int admin) {	
+	public static void synchronizeData(final String dateFrom, final String dateTo, final String tmList, final String nkList, 
+			final String maVt, final int userId, final String unit, final int admin) {	
 		try (
 			SessionFactory sessionMssqlFactory = HibernateUtils.getSessionFactory("hibernate-mssql.cfg.xml");
 			SessionFactory sessionMysqlFactory = HibernateUtils.getSessionFactory("hibernate-mysql.cfg.xml");) 
 		{
-			Session sesMssql = sessionMysqlFactory.openSession();
+			Session sesMssql = sessionMssqlFactory.openSession();
 			Session sesMysql = sessionMysqlFactory.openSession();
 			
 			Transaction tx = null;
 		    
-		    try {				
-		    	log.info("Start Session Factory...");
+		    try {
+		    	Long startTime = System.currentTimeMillis();
+		    	
 			    tx = sesMysql.beginTransaction();
-
-		    	Query query = sesMssql.createSQLQuery("CALL rpt_total_dhkd(:DateFrom, :DateTo, :tmList, :nkList, :ma_vt, :userId, :unit, :admin)")
+			    	
+		    	Query query = sesMssql.createSQLQuery("exec rpt_total_dhkd :DateFrom, :DateTo, :tmList, :nkList, :maVt, :userId, :unit, :admin")
 						  .addEntity(PurchaseOrder.class)
-						  .setParameter("DateFrom", dateFrom)
+		    			  .setParameter("DateFrom", dateFrom)
 						  .setParameter("DateTo", dateTo)
 						  .setParameter("tmList", tmList)
 						  .setParameter("nkList", nkList)
+						  .setParameter("maVt", maVt)
 						  .setParameter("userId", userId)
 						  .setParameter("unit", unit)
 						  .setParameter("admin", admin);
-				
+		    	
 				@SuppressWarnings("unchecked")
-				List<PurchaseOrder> puOrders = query.getResultList();
+				List<PurchaseOrder> results = query.getResultList();
 				
-				for (PurchaseOrder po : puOrders) {
-					sesMysql.persist(po);
+				final int numberOfRecords = results.size();
+				final int batchSize = 1000;
+				query = sesMysql.createQuery("delete from PurchaseOrder");
+				query.executeUpdate();
+				
+				for (int i = 0; i < numberOfRecords; i++) {
+					PurchaseOrder po = results.get(i);
+					sesMysql.save(po);
+					
+					if (i % batchSize == 0 && i != numberOfRecords) {
+						log.info("Flush a batch of INSERT & release memory: {} time(s)", (i / batchSize));
+						sesMysql.flush();
+						sesMysql.clear();
+					}
 				}
-			
-				log.info("Total Orders: {}", puOrders.size());
 			    tx.commit();
+			    Long endTime = System.currentTimeMillis();
+				log.info("Total processing: " + results.size());
+				log.info("Total processed time: " + (endTime - startTime) + " ms");
 			}
 			catch (Exception e) {
 			    if (tx != null ) tx.rollback();
